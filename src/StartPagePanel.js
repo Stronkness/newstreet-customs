a0884de12cbbe0c94c667239861b589ba0c0842f
andre.frisk.8218 <andre.frisk.8218@student.lu.se>
2022-08-19 01:07:54 +0200

    Change introduction text

--- src/StartPagePanel.js
+++ src/StartPagePanel.js
@@ -10,6 +10,8 @@ function StartPagePanel(){
                 <h2>Välkommen till Newstreet Customs!</h2>
                 <h3>Här hittar du alla byggen som skapats av Chris Frisk från hans tidigare år när det byggdes bilar.
                     <br/>
+                    Denna sida kommer att agera som en samling och dokumentation av byggstadiet och slutprodukten av fordonen.
+                    <br/>
                     Nedan eller i övre panelen finns det diverse sidor på byggen men även en sida om skaparen bakom Newstreet Customs.
                 </h3>
             </div>        
