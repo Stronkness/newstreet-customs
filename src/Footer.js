fc6369694007f7c7d06d9882c59ed09579e01616
André Frisk <andrefrisk98@gmail.com>
2022-10-11 14:38:08 +0200

    tester for gh pages

--- src/Footer.js
+++ src/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
       <br/>
       <div className="center">
         <div className="footer">
-          Newstreet Customs | Källkod finns tillgänglig på&#160;
+          TESTER HELLO Customs | Källkod finns tillgänglig på&#160;
             <a href="https://github.com/Stronkness/newstreet-customs">GitHub</a>
         </div>
       </div>
