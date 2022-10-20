932a4cb6c578f016a568eaef02bbc7537436f077
--unset <andrefrisk98@gmail.com>
2022-10-20 19:28:56 +0200

    Add more text to StartPagePanel

--- src/StartPagePanel.js
+++ src/StartPagePanel.js
@@ -9,14 +9,34 @@ function StartPagePanel(){
         <div>
             <div className="centerSpace">
                 <h2>Välkommen till Newstreet Customs!</h2>
-                <h3 className="smallerfont">Här hittar du alla byggen som skapats av Chris Frisk från hans tidigare år när det byggdes bilar.
+                <h3 className="smallerfont">
+                    Här hittar du alla dokumenterade byggen som skapats av Chris Frisk från hans tidigare år när det byggdes bilar.
                     <br/>
                     Denna sida kommer att agera som en samling och dokumentation av byggstadiet och slutprodukten av fordonen.
                     <br/>
-                    I sidopanelerna finns det diverse sidor på byggen och artiklar. För att veta mer om skaparen bakom  
+                    I sidopanelerna finns det diverse sidor på byggen och artiklar. 
                     <br/>
-                    Newstreet Customs klicka på länken i mitten av sidan.
+                    <br/>
+                    Chris började i tidig ålder att bygga och konstruera om mopeder. När körkortet var taget och
+                    <br/>
+                    bilintresset blev allt högre började Chris med att bygga om bilar och skapa nya fräsiga åk.
+                    <br/>
+                    som finns på denna sidan.
+
+                    <br/>
+                    <br/>
+                    Chris har även varit i ledningen för Team Nasty och har agerat som ledamot för gruppen.
+                    <br/>
+                    Team Nasty etablerade sig av ett kompisgäng i Österlen där de visade stort intrese för fräsiga åk och
+                    <br/>
+                    höga decibel på högtalarna. De anordnade tävlingar och uppvisningar på Österlen och lockade intresset
+                    <br/>
+                    för billjud hos både äldre och unga. Idag ligger Team Nasty på is men intresset för bilar och billjud
+                    <br/>
+                    finns än idag bland de gamla medlemmarna.
                 </h3>
+                <br/>
+                <br/>
                 <br/>
 
                 <div className="founder">
