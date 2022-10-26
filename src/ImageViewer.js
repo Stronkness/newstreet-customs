14c8662a0b0f5d3f6dec66d28db3eae9490524d1
André Frisk <andrefrisk98@gmail.com>
2022-10-26 15:49:03 +0200

    Pusgin working imageviewver

--- src/ImageViewer.js
+++ src/ImageViewer.js
@@ -1,48 +1,48 @@
-import React, { useState, useCallback } from "react";
-import ImageViewer from "react-simple-image-viewer";
+// import React, { useState, useCallback } from "react";
+// import ImageViewer from "react-simple-image-viewer";
 
-function ImgViewer() {
-  const [currentImage, setCurrentImage] = useState(0);
-  const [isViewerOpen, setIsViewerOpen] = useState(false);
-  const images = [props];
+// function ImgViewer(props) {
+//   const [currentImage, setCurrentImage] = useState(0);
+//   const [isViewerOpen, setIsViewerOpen] = useState(false);
+//   const images = [props];
 
-  const openImageViewer = useCallback((index) => {
-    setCurrentImage(index);
-    setIsViewerOpen(true);
-  }, []);
+//   const openImageViewer = useCallback((index) => {
+//     setCurrentImage(index);
+//     setIsViewerOpen(true);
+//   }, []);
 
-  const closeImageViewer = () => {
-    setCurrentImage(0);
-    setIsViewerOpen(false);
-  };
+//   const closeImageViewer = () => {
+//     setCurrentImage(0);
+//     setIsViewerOpen(false);
+//   };
+  
+//   return (
+//     <div>
+//       {images.map((src, index) => (
+//         <img
+//           src={src}
+//           onClick={() => openImageViewer(index)}
+//           width="300"
+//           key={index}
+//           style={{ margin: "2px" }}
+//           alt=""
+//         />
+//       ))}
 
-  return (
-    <div>
-      {images.map((src, index) => (
-        <img
-          src={src}
-          onClick={() => openImageViewer(index)}
-          width="300"
-          key={index}
-          style={{ margin: "2px" }}
-          alt=""
-        />
-      ))}
+//       {isViewerOpen && (
+//         <ImageViewer
+//           src={images}
+//           currentIndex={currentImage}
+//           onClose={closeImageViewer}
+//           disableScroll={false}
+//           backgroundStyle={{
+//             backgroundColor: "rgba(0,0,0,0.9)"
+//           }}
+//           closeOnClickOutside={true}
+//         />
+//       )}
+//     </div>
+//   );
+// }
 
-      {isViewerOpen && (
-        <ImageViewer
-          src={images}
-          currentIndex={currentImage}
-          onClose={closeImageViewer}
-          disableScroll={false}
-          backgroundStyle={{
-            backgroundColor: "rgba(0,0,0,0.9)"
-          }}
-          closeOnClickOutside={true}
-        />
-      )}
-    </div>
-  );
-}
-
-export default ImgViewer;
+// export default ImgViewer;
--- src/Unknown.js
+++ src/Unknown.js
@@ -1,11 +1,15 @@
-import React from "react";
-import ImgViewer from "./ImageViewer.js";
+import React, { useState, useCallback } from "react";
+// import ImgViewer from "./ImageViewer.js";
+
+import ImageViewer from "react-simple-image-viewer";
+
 
 import images from './Images.js';
 
 import './css/Center.css'
 
 const unknown_images = images[3];
+const render_images = exportImages();
 
 function exportImages(){
     const list_images = [];
@@ -19,16 +23,63 @@ function exportImages(){
         }
         list_images.push(unknown_images[png_string]);
     }
-
+    console.log(list_images)
     return list_images;
 }
+
+function ImgViewer() {
+    const [currentImage, setCurrentImage] = useState(0);
+    const [isViewerOpen, setIsViewerOpen] = useState(false);
+    const images = [];
+  
+    const openImageViewer = useCallback((index) => {
+      setCurrentImage(index);
+      setIsViewerOpen(true);
+    }, []);
+  
+    const closeImageViewer = () => {
+      setCurrentImage(0);
+      setIsViewerOpen(false);
+    };
+  
+    return (
+      <div>
+        {render_images.map((src, index) => (
+          <img
+            src={src}
+            onClick={() => openImageViewer(index)}
+            width="300"
+            key={index}
+            style={{ margin: "2px" }}
+            alt=""
+          />
+        ))}
+
+        {console.log(render_images)}
+
+        {isViewerOpen && (
+          <ImageViewer
+            src={render_images}
+            currentIndex={currentImage}
+            onClose={closeImageViewer}
+            disableScroll={false}
+            backgroundStyle={{
+              backgroundColor: "rgba(0,0,0,0.9)"
+            }}
+            closeOnClickOutside={true}
+          />
+        )}
+      </div>
+    );
+  }
 
 function Unknown(){
     const images = exportImages();
 
     return(
         <div className="center">
-            <ImgViewer images={images}/>
+            <ImgViewer/>
+            {/* <ImgViewer images={images}/> */}
             {console.log(Object.keys(unknown_images).length)}
             <img src={images[0]} alt={"Image not loaded ..."}/>
         </div>
